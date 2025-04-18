c602967f5221bb9ff106eb3eea13f5c8d52919a7
Ingu Lee <leeinnine@gmail.com>
2025-04-18 15:14:16 +0900

    Added: Different Learning Rate, Decay Schedule yaml/py files

--- hw3/cas4160/env_configs/__init__.py
+++ hw3/cas4160/env_configs/__init__.py
@@ -1,7 +1,13 @@
 from .dqn_atari_config import atari_dqn_config
 from .dqn_basic_config import basic_dqn_config
+from .dqn_basic_config_decay_fast import basic_dqn_config_decay_fast
+from .dqn_basic_config_decay_slow import basic_dqn_config_decay_slow
+from .dqn_basic_config_decay_slower import basic_dqn_config_decay_slower
 
 configs = {
     "dqn_atari": atari_dqn_config,
     "dqn_basic": basic_dqn_config,
+    "dqn_basic_decay_fast": basic_dqn_config_decay_fast,
+    "dqn_basic_decay_slow": basic_dqn_config_decay_slow,
+    "dqn_basic_decay_slower": basic_dqn_config_decay_slower,
 }
